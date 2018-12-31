e76a7b5d1dbf23864babd565c594a1374e44f209
myohtet <myohtet.github@gmail.com>
2018-12-31 21:41:45 +0630

    changes

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\products;
 
 class productController extends Controller
 {
     public function index(){
-    	
+
     }
 }
